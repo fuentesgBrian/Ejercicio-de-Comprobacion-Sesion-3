30cf05dbe073f82d20855b773d8b9dfc941818e0
fuentesgBrian <fuentesg.brian@gmail.com>
2023-04-14 17:27:08 -0400

    Ejercicio de Comprobacion Sesion 3 Mod3

---
+++ index.py
@@ -0,0 +1,12 @@
+
+# Definicion de variable a
+a = 20
+
+# Definicion de variable b
+b = 30
+
+# Variable resultado de la multiplicacion a y b
+resultado = a * b
+
+# Impresion del resultado de la multiplicacion 
+print(f"La multiplicacion de {a} x {b} es igual a {resultado}")
